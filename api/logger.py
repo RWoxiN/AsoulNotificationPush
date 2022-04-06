e50bd6b3a20a00a7c9fcf9d917b1025494bf2449
Rian Ng <rian.ng@icloud.com>
2022-04-06 19:18:12 +0800

    低pyhon版本问题标注

--- api/logger.py
+++ api/logger.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(
     filename='asoul.log',
     filemode='a',
-    encoding='utf-8',
+    encoding='utf-8', # 服务器中 python 版本过低需去掉该行，否则会报错
     format='%(asctime)s : %(levelname)s: {%(module)s} [%(funcName)s]: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.INFO
